82f75fe429288fb65cc53b0f82f37fdc8b22184c
Eric <stubbsguitar@gmail.com>
2023-05-22 14:31:03 -0600

    Fix error with category relational mappings, fix select boxes for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,9 @@
 class Category < ApplicationRecord
-  belongs_to :parent, :class_name => 'Category', optional: true
-  has_many :child_categories, :class_name => 'Category', :foreign_key => 'parent', inverse_of: :parent
+  has_many :child_categories, :class_name => 'Category', :foreign_key => 'parent', inverse_of: :parent_category
+  belongs_to :parent_category, :class_name => 'Category', optional: true, inverse_of: :child_categories
   has_many :items
+
+  def get_parent_name(parent_id)
+    Category.find_by(id: parent_id)&.name
+  end
 end
